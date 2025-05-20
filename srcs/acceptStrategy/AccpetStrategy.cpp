2711076b272c0534d2b81512a309b74f7e5aa716
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-05-20 19:02:11 +0400

    feat(acceptStrategy): implement handler

--- srcs/acceptStrategy/AccpetStrategy.cpp
+++ srcs/acceptStrategy/AccpetStrategy.cpp
@@ -10,4 +10,12 @@ AcceptStrategy::~AcceptStrategy()
 
 void AcceptStrategy::handleEvent(int fd __unused, PollManager &pollManager __unused, IRCServer &server __unused)
 {
+	struct sockaddr_in clientAddr;
+	socklen_t len = sizeof(clientAddr);
+	std::memset(&clientAddr, 0, len);
+	int clientFd = accept(fd, (struct sockaddr *)&clientAddr, &len);
+	if (clientFd < 0 || fcntl(clientFd, F_SETFL, O_NONBLOCK) < 0)
+		throw IRCException::ServerError(strerror(errno));
+	pollManager.addFd(clientFd, POLL_IN);
+	std::cout << "[INFO] Client " << clientFd << " connected" << std::endl;
 }
