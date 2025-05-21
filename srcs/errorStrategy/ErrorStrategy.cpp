eedb2f9d416bcfb198622222792a78fff9bc5ebb
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-05-21 23:35:34 +0400

    style[ErrorStrategy]: add space in error msg

--- srcs/errorStrategy/ErrorStrategy.cpp
+++ srcs/errorStrategy/ErrorStrategy.cpp
@@ -9,7 +9,7 @@ void ErrorStrategy::handleEvent(int fd, PollManager &pollManager, IRCServer &ser
 {
 	UserManager &userManager = server.getUserManager();
 	if (close(fd) < 0)
-		std::cerr << "[ERROR] Failed to close client " << fd << ": " << strerror(errno) << std::endl;
+		std::cerr << "[ERROR] Failed to close client " << fd << " : " << strerror(errno) << std::endl;
 	pollManager.removeFd(fd);
 	userManager.removeUser(fd);
 	std::cout << "[INFO] Client " << fd << " disconnected" << std::endl;
