b1493c00170aeb6bbadddb6cdb9086cea4b572c9
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-05-21 14:40:53 +0400

    feat[Poll Error]: implement error-close client event

--- srcs/errorStrategy/ErrorStrategy.cpp
+++ srcs/errorStrategy/ErrorStrategy.cpp
@@ -1,13 +1,16 @@
 #include <ErrorStrategy.hpp>
+#include <IRCServer.hpp>
 
-ErrorStrategy::ErrorStrategy()
-{
-}
+ErrorStrategy::ErrorStrategy() {}
 
-ErrorStrategy::~ErrorStrategy()
-{
-}
+ErrorStrategy::~ErrorStrategy() {}
 
-void ErrorStrategy::handleEvent(int fd __unused, PollManager &pollManager __unused, IRCServer &server __unused)
+void ErrorStrategy::handleEvent(int fd, PollManager &pollManager, IRCServer &server)
 {
+	UserManager &userManager = server.getUserManager();
+	if (close(fd) < 0)
+		std::cerr << "[ERROR] Failed to close client " << fd << ": " << strerror(errno) << std::endl;
+	pollManager.removeFd(fd);
+	userManager.removeUser(fd);
+	std::cout << "[INFO] Client " << fd << " disconnected" << std::endl;
 }
