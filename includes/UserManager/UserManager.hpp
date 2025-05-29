da80313fb1dc19d0e8e44cd084d0cbccf279ca48
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-05-29 13:19:01 +0400

    feat[UserManager]: add print method

--- includes/UserManager/UserManager.hpp
+++ includes/UserManager/UserManager.hpp
@@ -17,6 +17,19 @@ public:
 	User *getUserByFd(int fd);
 	User *getUserByNick(const String &nick);
 	bool changeNick(const String &nickname, User *user);
+	void print()
+	{
+		std::map<int, User *>::iterator it = _usersByFd.begin();
+		for (; it != _usersByFd.end(); ++it)
+		{
+			std::cout << it->first << "\t" << it->second << std::endl;
+		}
+		std::map<String, User *>::iterator it2 = _usersByNick.begin();
+		for (; it2 != _usersByNick.end(); ++it2)
+		{
+			std::cout << it2->first << "\t" << it2->second << std::endl;
+		}
+	}
 };
 
 #endif
