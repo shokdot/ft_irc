2a6803c16b4c14d0be096f0d9cc923c64ad695f0
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-06-02 20:42:11 +0400

    fix[Utils]: add missed header

--- includes/Utils/Utils.hpp
+++ includes/Utils/Utils.hpp
@@ -14,6 +14,7 @@ public:
 	static void sendWrapper(const String &str, int sock_fd);
 	static struct sockaddr_in createSockStruct(sa_family_t family, in_port_t port, in_addr_t addr);
 	static struct pollfd createPollStruct(int fd, short events);
+	static std::vector<String> splitByDelim(const String &line, char delimeter);
 };
 
 #endif
