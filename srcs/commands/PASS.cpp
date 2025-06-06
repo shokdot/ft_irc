c584a9ad659c6f9fb82cf0ffca3e9446639ef737
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-06-06 12:01:34 +0400

    fix[PASS]: some minor logic bug, feat[REPLY]: add main replies

--- srcs/commands/PASS.cpp
+++ srcs/commands/PASS.cpp
@@ -1,5 +1,6 @@
 #include <PASS.hpp>
 #include <IRCServer.hpp>
+#include <Replies.hpp>
 
 PASS::PASS()
 {
@@ -11,34 +12,28 @@ PASS::~PASS()
 
 void PASS::execute(Client *client, CmdStruct &cmd, IRCServer &server)
 {
-	int fd = client->getClientFd();
-	if (cmd.params.empty())
-	{
-		std::string reply = ":localhost 461 pass :Not enough parameters\r\n";
-		Utils::sendWrapper(reply, fd);
-		return;
-	}
-	if (cmd.params[0] != server.getPass())
-	{
-		std::string reply = ":localhost 464 :Password incorrect\r\n";
-		Utils::sendWrapper(reply, fd);
-		return;
-	}
 	if (!client)
+		return;
+
+	int fd = client->getClientFd();
+
+	if (client->getAuth())
 	{
-		std::string reply = "Something went wrong\r\n";
-		Utils::sendWrapper(reply, fd);
+		Utils::sendReply(Reply::ERR_ALREADYREGISTRED(client->getNickname()), fd);
 		return;
 	}
-	else
+	else if (cmd.params.empty())
 	{
-		if (client->getAuth())
-		{
-			std::string reply = ":localhost 462 " + client->getNickname() + " :You may not reregister\r\n";
-			Utils::sendWrapper(reply, fd);
-			return;
-		}
-		client->setAuth(true);
-		std::cout << "[INFO] Client " << fd << " authorized" << std::endl;
+		Utils::sendReply(Reply::ERR_NEEDMOREPARAMS(client->getNickname(), "PASS"), fd);
+		return;
 	}
+	else if (cmd.params[0] != server.getPass())
+	{
+		Utils::sendReply(Reply::ERR_PASSWDMISMATCH(client->getNickname(), "incorrect"), fd);
+		server.getEventDispatcher().disconnectClient(fd, server);
+		return;
+	}
+
+	client->setAuth(true);
+	std::cout << "[INFO] Client " << fd << " authorized" << std::endl;
 }
