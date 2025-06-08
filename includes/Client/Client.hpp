b9659dcbb539004e6f2aebbfb25150c192cf2f91
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-06-08 13:10:32 +0400

    style[Client]: order methods

--- includes/Client/Client.hpp
+++ includes/Client/Client.hpp
@@ -25,28 +25,30 @@ public:
 	void setRealname(const String &realname);
 	void setHostname(const String &hostname);
 	void setAuth(bool auth);
+	void setIsQuitting(bool flag);
+	void setIsRegistered(bool flag);
 
+	int getClientFd() const;
 	const String &getUsername() const;
 	const String &getNickname() const;
 	const String &getRealname() const;
 	const String &getHostname() const;
-	int getClientFd() const;
-	bool getAuth() const;
-	bool isRegistered() const;
-	void joinChannel(Channel *channel);
-	void removeChannel(Channel *channel);
 	bool isJoinedChannel(Channel *channel);
 	std::set<Channel *> &getJoinedChannels();
-	void broadcastJoinedChannels(const String &msg);
-	String getPrefix() const;
+	bool getAuth() const;
+	bool isRegistered() const;
 	bool isQuitting();
-	void setIsQuitting(bool flag);
-	void setIsRegistered(bool flag);
+
+	void joinChannel(Channel *channel);
+	void removeChannel(Channel *channel);
+	void broadcastJoinedChannels(const String &msg);
+	void sendWelcome(const String &time);
+	String getPrefix() const;
+
 	bool hasNick();
 	bool hasUser();
 
 	bool isFirstLogin();
-	void sendWelcome(const String &time);
 };
 
 #endif
